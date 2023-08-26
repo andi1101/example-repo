ee0de4f0583b2d1b558112fa898e0ebce8377c59
root <chircaandi@yahoo.com>
2023-08-26 19:42:52 -0400

    add python - 12 bis

---
+++ var.py
@@ -0,0 +1,46 @@
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+[root@python w3docs]# cat zz2_if-else-while_andi.py
+# If statement
+x = 10
+if x > 0:
+    print("x is positive")
+elif x == 0:
+    print("x is zero")
+else:
+    print("x is negative")
+
+# For loop
+for i in range(1, 11):
+    print(i)
+
+
+
+
+# While loop
+i = 1
+while i <= 10:
+ print(i)
+ i += 1
+
+
+
+
+
+
+
+
+
+# Functions
+
+# Define a function
+def square(x):
+    return x ** 2
+
+# Call the function
+print(square(5))
+
